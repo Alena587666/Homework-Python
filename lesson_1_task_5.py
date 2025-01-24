f92ca271d19c1a8db46fcf50a52d09efe4780cc8
Alena <a31418520@gmail.com>
2025-01-24 14:06:00 +0300

    HW1

---
+++ lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_parameter(num):
+    print(num, end='')
+
+
+print_parameter(8)
+print_parameter(8)
+print_parameter(0)
+print_parameter(0)
+print_parameter(5)
+print_parameter(5)
+print_parameter(5)
+print_parameter(3)
+print_parameter(5)
+print_parameter(3)
+print_parameter(5)
