8cd37ef35d4a4608fa27c418d2f24d36496c9028
Alena <a31418520@gmail.com>
2025-02-15 13:11:08 +0300

    HW4

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -3,29 +3,5 @@ from string_utils import StringUtils
 
 string_utils = StringUtils()
 
-@pytest.mark.parametrize("string",
-  [ 
-    ("москва", "Москва"),
-    ("николай", "Николай"),
-    ("skypro", "Skypro"),
-  ],
-)
-
-def test_capitilize_positive(string: str) -> str:
-  string = StringUtils()  
-  assert string.capitilize(string)
-
-@pytest.mark.parametrize("string",
-  [ 
-    ("  Mосква", "Москва"),
-    (" Hиколай", "Николай"),
-    ("  Skypro", "Skypro"),
-  ],
-)
-
-def test_trim_positive(string: str) -> str:
-  string = StringUtils()
-  whitespace = " "
-  while string.startswith(whitespace):
-        string = string.removeprefix(whitespace)
-  assert string
+def test_capitalize_positive():
+    assert string_utils.capitalize("skypro") == ["Skypro"]
