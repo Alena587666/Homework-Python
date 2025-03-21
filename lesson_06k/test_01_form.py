460628769ba26e293efb74da856f4eb60a520459
Alena <a31418520@gmail.com>
2025-03-21 20:32:32 +0300

    HW6k

---
+++ lesson_06k/test_01_form.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_form():
+
+    driver = webdriver.Chrome()
+
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+
+    driver.find_element(By.NAME,"first-name").send_keys("Иван")
+    driver.find_element(By.NAME,"last-name").send_keys("Петров")
+    driver.find_element(By.NAME,"address").send_keys("Ленина,55-3")
+    driver.find_element(By.NAME,"e-mail").send_keys("test@skypro.com")
+    driver.find_element(By.NAME,"phone").send_keys("+7985899998787")
+    driver.find_element(By.NAME,"zip-code").send_keys("")
+    driver.find_element(By.NAME,"city").send_keys("Москва")
+    driver.find_element(By.NAME,"country").send_keys("Россия")
+    driver.find_element(By.NAME,"job-position").send_keys("QA")
+    driver.find_element(By.NAME,"company").send_keys("SkyPro")
+
+    driver.find_element(By.XPATH,'//button[text() = "Submit"]').click()
+
+    assert "success" in driver.find_element(By.ID,"first-name").get_attribute("class")
+    assert "success" in driver.find_element(By.ID,"last-name").get_attribute("class")
+    assert "success" in driver.find_element(By.ID,"address").get_attribute("class")
+    assert "success" in driver.find_element(By.ID,"e-mail").get_attribute("class")
+    assert "success" in driver.find_element(By.ID,"phone").get_attribute("class")
+    assert "success" in driver.find_element(By.ID,"zip-code").get_attribute("class")
+    assert "success" in driver.find_element(By.ID,"city").get_attribute("class")
+    assert "success" in driver.find_element(By.ID,"country").get_attribute("class")
+    assert "success" in driver.find_element(By.ID,"job-position").get_attribute("class")
+    assert "success" in driver.find_element(By.ID,"company").get_attribute("class")
+
+    assert "danger" in driver.find_element(By.ID,"zip-code").get_attribute("class")
